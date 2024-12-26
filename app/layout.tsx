54ff706fc3eca10eb124b0db5a9a1f69d0f4ef3b
Gabriel <gabriel.bachelor@gmail.com>
2024-12-26 15:05:12 +0000

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+// import { inter } from '@/app/ui/fonts';
 import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
 
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 
 
@@ -17,3 +27,6 @@ export default function RootLayout({
       </html>
   );
 }
+
+
+
