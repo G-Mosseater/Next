3e0f4f3c1595c0b6e72bc50e69d48272e27dfd1c
Gabriel <gabriel.bachelor@gmail.com>
2024-12-26 16:35:11 +0000

    x

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
-import path from 'path';
-
-// Use path.resolve if you're accessing the file system or paths
-const directoryPath = path.resolve();
-console.log(directoryPath); // Logs the directory path
-
+ 
 export default NextAuth(authConfig).auth;
-
+ 
 export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
